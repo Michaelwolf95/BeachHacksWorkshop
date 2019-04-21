4330e7bcc9a68e43e24223a703f9478208ef79eb
Michaelwolf95 <michaelwolf95@gmail.com>
2019-04-20 21:41:53 -0700

    Initial Project Commit

---
+++ MyFirstUnityProject/Assets/Scripts/DudeMover.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DudeMover : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpForce = 10f;
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 currentPosition = transform.position;
+
+        float deltaX = (horizontalInput * speed * Time.deltaTime);
+
+        transform.position = new Vector3(
+            currentPosition.x + deltaX,
+            currentPosition.y,
+            currentPosition.z);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("JUMP!");
+            Rigidbody2D myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+            if(myRigidbody != null)
+            {
+                myRigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+
+        }
+
+    }
+}
